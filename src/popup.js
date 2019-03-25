2b9bf5022971a6e742efecb2b853e4b4cd5d6ea1
Caolan McMahon <caolan.mcmahon@gmail.com>
2019-03-25 13:52:24 +0000

    Separate production target without test suite, closes #7

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,13 @@
-
-import browser from './browser.js'
+import browser from './browser.js';
 
 document.getElementById('reset').onclick = function(el) {
-  browser.runtime.reload()
-}
-document.getElementById('test').onclick = function(el) {
-  window.open(browser.extension.getURL("test.html"), '_blank')
+    browser.runtime.reload();
+};
+
+if (DEVELOPMENT) {
+    document.getElementById('test').onclick = function(el) {
+        window.open(browser.extension.getURL("test.html"), '_blank');
+    };
+} else {
+    document.body.removeChild(document.getElementById('test'));
 }
