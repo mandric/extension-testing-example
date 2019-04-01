64855dbd1a0f16ab97645199178ef01fda957a48
Caolan McMahon <caolan.mcmahon@gmail.com>
2019-04-01 11:41:03 +0100

    Use redux for state module

--- src/state.js
+++ src/state.js
@@ -1,55 +1,79 @@
+import { createStore } from 'redux';
+
 /*
  * data store
  */
-const data = {
+
+const initial = {
   fibSeq: [],
   tabs: {}
-}
+};
+
+const store = createStore((state = initial, action) => {
+    switch (action.type) {
+    case 'CALC_NEXT':
+        const val = calcNextVal(state.fibSeq);
+        return setVal(state, val, action.id);
+    case 'UNSET':
+        delete state.tabs[action.id];
+        return state;
+    case 'RESET':
+        state.fibSeq = [];
+        state.tabs = {};
+        return state;
+    default:
+        return state;
+    }
+});
 
 /*
  * Save fib sequence value to the tabs map and fibSeq array.
  */
-const setVal = (val, id) => {
-  data.tabs[id] = val
-  data.fibSeq.push(val)
-}
+const setVal = (state, val, id) => {
+    state.tabs[id] = val;
+    state.fibSeq.push(val);
+    return state;
+};
 
 /*
  * Initialize fib sequence on first two values, then calculate after that.
  */
-const calcNextVal = () => {
-  let next = 0
-  if (data.fibSeq.length === 0) {
-    return 0
-  } else if (data.fibSeq.length === 1) {
-    return 1
-  }
-  data.fibSeq.slice(-2).map(val => next += val);
-  return next
-}
+const calcNextVal = (fibSeq) => {
+    let next = 0;
+    if (fibSeq.length === 0) {
+      return 0;
+    } else if (fibSeq.length === 1) {
+        return 1;
+    }
+    fibSeq.slice(-2).map(val => next += val);
+    return next;
+};
 
 const getVal = (id) => {
-  return data.tabs[id]
-}
+    return store.getState().tabs[id];
+};
 
 const nextVal = (id) => {
-  const val = calcNextVal()
-  setVal(val, id)
-  return val
-}
+    store.dispatch({
+        type: 'CALC_NEXT',
+        id: id
+    });
+};
 
 const unset = (id) => {
-  delete data.tabs[id]
-}
+    store.dispatch({
+        type: 'UNSET',
+        id: id
+    });
+};
 
 const reset = () => {
-  data.fibSeq = []
-  data.tabs = {}
-}
+    store.dispatch({type: 'RESET'});
+};
 
 export default {
-  getVal,
-  nextVal,
-  unset,
-  reset
-}
+    getVal,
+    nextVal,
+    unset,
+    reset
+};
